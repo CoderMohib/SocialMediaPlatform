1b3924f82aac2d8ff704c03a2ed44b392ab441b5
CoderMohib <alimohib025@gmail.com>
2024-11-19 14:23:28 +0500

    Separate procedures from this file

--- func.sql
+++ func.sql
@@ -27,19 +27,7 @@ from Users
 WHERE UserID=@UserID)
 END;
 
-GO
-CREATE OR ALTER PROCEDURE InsertUser
-    @Username VARCHAR(50),
-    @Email VARCHAR(100),
-    @PasswordF VARCHAR(255),
-    @Birthdate DATE,
-    @FirstName VARCHAR(50),
-    @LastName VARCHAR(50)
-AS
-INSERT INTO Users
-    (Username, Email, PasswordF, DateOfBirth ,FirstName ,LastName)
-VALUES
-    (@Username, @Email, @PasswordF, @Birthdate , @FirstName, @LastName);
+
 GO
 CREATE OR ALTER FUNCTION retriveDataUserName (@Username VARCHAR(50))
 RETURNS TABLE
@@ -88,58 +76,7 @@ RETURN
 FROM Posts
 WHERE UserID = @UserID;
 
-GO
-CREATE OR ALTER PROCEDURE updateAccountStatus
-    @UserID INT,
-    @AccountStatus VARCHAR(20)
-AS
-UPDATE Users
-SET AccountStatus = @AccountStatus, UpdatedAt = GETDATE()
-WHERE UserID= @UserID
 
-GO
-CREATE OR ALTER PROCEDURE updateUserName
-    @UserID INT,
-    @Username VARCHAR(50)
-AS
-UPDATE Users
-SET Username  = @Username,UpdatedAt = GETDATE()
-WHERE UserID= @UserID
-GO
-CREATE OR ALTER PROCEDURE updatePassWord
-    @UserID INT,
-    @PasswordF VARCHAR(255)
-AS
-UPDATE Users
-SET PasswordF=@PasswordF, UpdatedAt = GETDATE()
-WHERE UserID= @UserID
-
-GO
-CREATE OR ALTER PROCEDURE updateName
-    @UserID INT,
-    @Fname VARCHAR(50),
-    @Lname VARCHAR(50)
-AS
-UPDATE Users
-SET FirstName=@Fname, LastName = @Lname, UpdatedAt = GETDATE()
-WHERE UserID= @UserID
-GO
-CREATE OR ALTER PROCEDURE DelUser
-    @UserID INT
-AS
-DELETE FROM Users WHERE UserID = @UserID;
-GO
------------------------------------------------------
-CREATE OR ALTER PROCEDURE inputPosts
-@UserID INT,
-@Content Text
-AS
-    INSERT INTO Posts(UserID,Content) VALUES (@UserID,@Content);
-GO
-CREATE OR ALTER PROCEDURE DelPost
-    @UserID INT
-AS
-DELETE FROM Posts WHERE UserID = @UserID;
 GO
 
 CREATE OR ALTER FUNCTION getUserPost(@UserID INT)
@@ -147,18 +84,6 @@ RETURNS TABLE
 AS
 RETURN( SELECT PostID,Content,CreatedAt from Posts WHERE UserID = @UserID );
 
-GO
-CREATE OR ALTER PROCEDURE DelLikes
-    @UserID INT
-AS
-DELETE FROM Likes WHERE UserID = @UserID;
-GO
-CREATE OR ALTER FUNCTION getLikes(@PostID INT)
-RETURNS INT
-AS
-BEGIN
-RETURN(SELECT COUNT(UserID) from Likes WHERE PostID = @PostID AND UserID IS NOT NULL)
-END
 
 GO
 
@@ -169,9 +94,5 @@ BEGIN
 RETURN(SELECT COUNT(UserID) from Comments WHERE PostID = @PostID AND UserID IS NOT NULL)
 END
 
-GO
-CREATE OR ALTER PROCEDURE DelComments
-    @UserID INT
-AS
-DELETE FROM Comments WHERE UserID = @UserID;
+
 GO
